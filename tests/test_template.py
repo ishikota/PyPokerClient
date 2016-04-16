5612fc3f6a89da9d0e9cab5ee9a1c98e3700b17f
Kota Ishimoto <ishikota086@gmail.com>
2016-04-16 18:24:47 +0900

    Create test template

---
+++ tests/test_template.py
@@ -0,0 +1,11 @@
+import unittest
+from nose.tools import *
+#from poker_client. import 
+
+class TemplateTest(unittest.TestCase):
+
+  def setUp(self):
+    pass
+
+if __name__ == '__main__':
+  unittest.main()
