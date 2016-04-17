cef0ffd1a2211db86c85ca4429aef12a9aa5acea
Kota Ishimoto <ishikota086@gmail.com>
2016-04-17 23:55:28 +0900

    Create PokerPhaseHandler

---
+++ poker_client/poker_phase_handler.py
@@ -0,0 +1,47 @@
+import json
+
+class PokerPhaseHandler:
+
+  # state
+  PLAY_POKER = 4
+
+  def __init__(self, params_builder, poker_algorithm):
+    self.pb = params_builder
+    self.pa = poker_algorithm
+
+  # Return next state
+  def on_message(self, state, ws, msg):
+    return self.switch_action_by_message(json.loads(msg), state, ws)
+
+  # FIXIT doing side effect operation (message, websocket.send)
+  def switch_action_by_message(self, msg, state, ws):
+    if self.type_ping(msg):
+      return self.retry_request_if_needed(ws, state)
+
+    if self.type_ask(msg['message']):
+      act_data = self.pa.receive_data(msg["message"]["data"])
+      self.declare_action(ws, act_data)
+    elif self.type_notification(msg['message']):
+      self.pa.receive_data(msg["message"]["data"])
+
+    return state
+
+  def retry_request_if_needed(self, ws, state):
+    # TODO Implment
+    return state
+
+  def declare_action(self, ws, act_data):
+    ws.send(self.pb.build_declare_action_params(act_data))
+
+  def type_ping(self, msg):
+    return msg['identifier'] == '_ping'
+
+  def type_ask(self, msg):
+    return msg['phase'] == 'play_poker' and msg['type'] == 'ask'
+
+  def type_notification(self, msg):
+    return msg['phase'] == 'play_poker' and msg['type'] == 'notification'
+
+  def forward_state(self, state):
+    return state + 1
+
---
+++ tests/poker_phase_handler_test.py
@@ -0,0 +1,93 @@
+from base_unittest import BaseUnitTest
+import json
+from mock import Mock
+
+from nose.tools import *
+from poker_client.poker_phase_handler import PokerPhaseHandler
+from poker_client.params_builder import ParamsBuilder
+
+class WantedPhaseHandlerTest(BaseUnitTest):
+
+  def setUp(self):
+    self.pb = self.params_builder_mock()
+    self.pa = self.algo_mock()
+    self.ph = PokerPhaseHandler(self.pb, self.pa)
+
+  def test_switch_action_when_ask(self):
+    ws = self.websocket_spy()
+    msg = self.ask()
+    state = PokerPhaseHandler.PLAY_POKER
+
+    next_state = self.ph.switch_action_by_message(msg, state, ws)
+
+    pa_args = self.pa.receive_data.call_args_list[0][0][0]
+    ws_args = ws.send.call_args_list[0][0][0]
+    self.eq(PokerPhaseHandler.PLAY_POKER, next_state)
+    self.eq(msg["message"]["data"], pa_args)
+    self.eq(self.mock_declare_action_msg(), ws_args)
+
+  def test_switch_action_when_notification(self):
+    ws = self.websocket_spy()
+    msg = self.notification()
+    state = PokerPhaseHandler.PLAY_POKER
+
+    next_state = self.ph.switch_action_by_message(msg, state, ws)
+
+    pa_args = self.pa.receive_data.call_args_list[0][0][0]
+    self.eq(PokerPhaseHandler.PLAY_POKER, next_state)
+    self.eq(msg["message"]["data"], pa_args)
+    self.eq(ws.send.call_count, 0)
+
+  def test_retry_request_if_needed(self):
+    pass # TODO
+
+  def test_type_ping(self):
+    self.true(self.ph.type_ping(self.ping()))
+    self.false(self.ph.type_ping(self.ask()))
+
+  def test_type_ask(self):
+    self.true(self.ph.type_ask(self.ask()["message"]))
+    self.false(self.ph.type_ask(self.notification()["message"]))
+
+  def test_type_notification(self):
+    self.true(self.ph.type_notification(self.notification()["message"]))
+    self.false(self.ph.type_notification(self.ask()["message"]))
+
+  def test_forward_state(self):
+    self.eq(2, self.ph.forward_state(1))
+    self.eq(3, self.ph.forward_state(self.ph.forward_state(1)))
+
+  def ping(self):
+    return json.loads('{"identifier":"_ping","message":1460779289}')
+
+  def ask(self):
+    return json.loads(r'{"identifier":"{\"channel\":\"RoomChannel\"}", "message" : { "phase":"play_poker", "type":"ask", "data":"hoge" } }')
+
+  def notification(self):
+    return json.loads(r'{"identifier":"{\"channel\":\"RoomChannel\"}", "message" : { "phase":"play_poker", "type":"notification", "data":"fuga" } }')
+
+
+  def websocket_spy(self):
+    websocket = Mock()
+    websocket.send.return_value = None
+    return websocket
+
+  def params_builder_mock(self):
+    pb = Mock()
+    pb.build_declare_action_params.return_value = self.mock_declare_action_msg()
+    return pb
+
+  def algo_mock(self):
+    algo = Mock()
+    algo.receive_data.return_value = self.mock_algo_return()
+    return algo
+
+  def mock_declare_action_msg(self):
+    return "declare_action"
+
+  def mock_algo_return(self):
+    return "some_action"
+
+if __name__ == '__main__':
+  unittest.main()
+
