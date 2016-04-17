e55e1a128b3bf76b4055b6876fd7a54f1af0bb13
Kota Ishimoto <ishikota086@gmail.com>
2016-04-17 12:11:02 +0900

    Test params_builder

---
+++ tests/params_builder_test.py
@@ -0,0 +1,33 @@
+import unittest
+import json
+from nose.tools import *
+
+from poker_client.params_builder import ParamsBuilder
+
+class ParamsBuilderTest(unittest.TestCase):
+
+  def setUp(self):
+    self.player_id = 53
+    self.room_id = 7
+    self.credential = 'a' * 22
+    self.pb = ParamsBuilder(self.player_id, self.room_id, self.credential)
+
+  def test_build_subscribe_param(self):
+    p = json.loads(self.pb.build_subscribe_params())
+    self.assertEqual(p["command"], "subscribe")
+
+  def test_build_my_params(self):
+    p = json.loads(self.pb.build_my_params("c"))
+    d = json.loads(p["data"])
+    self.assertEqual(p["command"], "c")
+    self.assertEqual(p["identifier"], self.room_identifier())
+    self.assertEqual(d["player_id"], self.player_id)
+    self.assertEqual(d["room_id"], self.room_id)
+    self.assertEqual(d["credential"], self.credential)
+
+  def room_identifier(self):
+    return r'{"channel":"RoomChannel"}'
+
+if __name__ == '__main__':
+  unittest.main()
+
